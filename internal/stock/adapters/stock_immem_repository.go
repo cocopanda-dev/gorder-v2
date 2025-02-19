b7a5a87244218e6f6267982ab30560f9562f9e55
cocopanda-dev <fotdw1991@hotmail.com>
2025-02-19 01:14:59 -0500

    setup order/stock in memory store logic

---
+++ internal/stock/adapters/stock_immem_repository.go
@@ -0,0 +1,52 @@
+package adapters
+
+import (
+	"context"
+	"sync"
+
+	"github.com/cocopanda-dev/gorder-v2/common/genproto/orderpb"
+	domain "github.com/cocopanda-dev/gorder-v2/stock/domain/stock"
+)
+
+type MemoryStockRepository struct {
+	lock  *sync.RWMutex
+	store map[string]*orderpb.Item
+}
+
+var stub = map[string]*orderpb.Item{
+	"item_id": {
+		Id:       "foo_item",
+		Name:     "stub item",
+		Quantity: 10000,
+		PriceID:  "stub_item_price_id",
+	},
+}
+
+func NewMemoryOrderRepository() *MemoryStockRepository {
+	return &MemoryStockRepository{
+		lock:  &sync.RWMutex{},
+		store: stub,
+	}
+}
+func (m MemoryStockRepository) GetItems(ctx context.Context, ids []string) ([]*orderpb.Item, error) {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
+	var (
+		res     []*orderpb.Item
+		missing []string
+	)
+	for _, id := range ids {
+		if item, exist := m.store[id]; exist {
+			res = append(res, item)
+		} else {
+			missing = append(missing, id)
+		}
+	}
+
+	if len(res) == len(ids) {
+		return res, nil
+	}
+
+	return res, domain.NotFoundError{Missing: missing}
+}
